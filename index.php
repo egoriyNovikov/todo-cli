f2decc8aaa2affd2c9ac9426863a9ec568b44467
Egor Novikov <egoriyNovikov@gmail.com>
2026-03-16 18:53:29 +0300

    get catch errors in index.php

--- index.php
+++ index.php
@@ -7,8 +7,18 @@ use EgorNovikov\TodoCli\Database\Database;
 require_once __DIR__ . '/vendor/autoload.php';
 
 $databaseConfig = require __DIR__ . '/config/database.php';
+
+try {
 $db = new Database($databaseConfig);
+} catch (PDOException $e) {
+  echo "\033[31mОшибка при подключении к базе данных: " . $e->getMessage() . "\033[0m\n";
+  exit(1);
+}
 
-$input = new ArgvInput($argv);
-
-(new App($input, $db))->run();
+try {
+  $input = new ArgvInput($argv);
+  (new App($input, $db))->run();
+} catch (Exception $e) {
+  echo "\033[31mОшибка: " . $e->getMessage() . "\033[0m\n";
+  exit(1);
+}
